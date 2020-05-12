a08593e6c3cfb770758e69b827a05886eb4c6d8c
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-05-12 20:59:45 +0200

    Fix cli

--- backend/database_manager/cli.py
+++ backend/database_manager/cli.py
@@ -15,8 +15,8 @@ def main() -> None:
         with DatabaseManager(
             DB_MANAGER_LISTENING,
             DB_MANAGER_PORT,
-            WORKLOAD_PUBSUB_PORT,
             WORKLOAD_SUB_HOST,
+            WORKLOAD_PUBSUB_PORT,
         ) as database_manager:
             print(
                 f"Database manager running on port {DB_MANAGER_PORT} (Press CTRL+C to quit)"
