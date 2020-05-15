f980bd8fadb2e70401ab17ab3390217863a7002f
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-05-15 17:17:35 +0200

    Refactor latency benchmark

--- benchmark_tools/latency/latency_benchmark.py
+++ benchmark_tools/latency/latency_benchmark.py
@@ -12,7 +12,7 @@ from benchmark_tools.latency.curl_wrapper import (
     stop_workload,
 )
 
-RUNS = 100
+RUNS = 10
 
 GET_ENDPOINTS = [
     "workload",
@@ -24,99 +24,146 @@ GET_ENDPOINTS = [
 ]
 
 
+def print_green(value):
+    """Print green colored text."""
+    print("\033[92m{}\033[00m".format(value))
+
+
+def print_cyan(value):
+    """Print cyan colored text."""
+    print("\033[96m{}\033[00m".format(value))
+
+
+def print_data(endpoint, results):
+    print_green(f"\nResults for {endpoint}")
+    print_cyan("Server process time")
+    print(f"Avg: {mean(results['server_process_times']) * 1_000}ms")
+    print(f"Median: {median(results['server_process_times']) * 1_000}ms")
+    print_cyan("Name lookup time")
+    print(f"Avg: {mean(results['name_lookup_times']) * 1_000}ms")
+    print(f"Median: {median(results['name_lookup_times']) * 1_000}ms")
+    print_cyan("Connect time")
+    print(f"Avg: {mean(results['connect_times']) * 1_000}ms")
+    print(f"Median: {median(results['connect_times']) * 1_000}ms\n")
+
+
 def get_endpoints():
-    for enpoint in GET_ENDPOINTS:
-        print(f"Run on {enpoint}")
+    enpoint_results = {}
+    for endpoint in GET_ENDPOINTS:
         server_process_times = []
+        name_lookup_times = []
+        connect_times = []
         for _ in range(RUNS):
-            results = execute_get(enpoint)
+            results = execute_get(endpoint)
             server_process_times.append(results["total"] - results["pretransfer"])
-        print(f"Avg: {mean(server_process_times) * 1_000}ms")
-        print(f"Median: {median(server_process_times) * 1_000}ms")
+            name_lookup_times.append(results["namelookup"])
+            connect_times.append(results["connect"] - results["namelookup"])
+        enpoint_results[endpoint] = {
+            "server_process_times": server_process_times,
+            "name_lookup_times": name_lookup_times,
+            "connect_times": connect_times,
+        }
+    return enpoint_results
 
 
-def add_delete_database():
-    add_server_process_times = []
-    delete_server_process_times = []
+def post_delete_on_endpoint(
+    endpoint, post_endpoint_handler, delete_enpoint_handler, handler_argument=None
+):
+    post_to_server_process_times = []
+    post_to_name_lookup_times = []
+    post_to_connect_times = []
+    delete_to_server_process_times = []
+    delete_to_name_lookup_times = []
+    delete_to_connect_times = []
+
     for _ in range(RUNS):
-        add_result = add_database("db")
-        add_server_process_times.append(add_result["total"] - add_result["pretransfer"])
-        delete_result = delete_database("db")
-        delete_server_process_times.append(
+        if handler_argument:
+            post_result = post_endpoint_handler(handler_argument)
+        else:
+            post_result = post_endpoint_handler()
+        post_to_server_process_times.append(
+            post_result["total"] - post_result["pretransfer"]
+        )
+        post_to_name_lookup_times.append(post_result["namelookup"])
+        post_to_connect_times.append(post_result["connect"] - post_result["namelookup"])
+        if handler_argument:
+            delete_result = delete_enpoint_handler(handler_argument)
+        else:
+            delete_result = delete_enpoint_handler()
+        delete_to_server_process_times.append(
             delete_result["total"] - delete_result["pretransfer"]
         )
-
-    print("Run add database")
-    print(f"Avg: {mean(add_server_process_times) * 1_000}ms")
-    print(f"Median: {median(add_server_process_times) * 1_000}ms")
-
-    print("Run delete database")
-    print(f"Avg: {mean(delete_server_process_times) * 1_000}ms")
-    print(f"Median: {median(delete_server_process_times) * 1_000}ms")
-
-
-def start_stop_worker():
-    add_database("db")
-    start_server_process_times = []
-    stop_server_process_times = []
-    for _ in range(RUNS):
-        start_result = start_worker()
-        start_server_process_times.append(
-            start_result["total"] - start_result["pretransfer"]
-        )
-        stop_result = stop_worker()
-        stop_server_process_times.append(
-            stop_result["total"] - stop_result["pretransfer"]
-        )
-    delete_database("db")
-
-    print("Run start worker")
-    print(f"Avg: {mean(start_server_process_times) * 1_000}ms")
-    print(f"Median: {median(start_server_process_times) * 1_000}ms")
-
-    print("Run stop worker")
-    print(f"Avg: {mean(stop_server_process_times) * 1_000}ms")
-    print(f"Median: {median(stop_server_process_times) * 1_000}ms")
-
-
-def start_stop_workload():
-    start_server_process_times = []
-    stop_server_process_times = []
-    for _ in range(RUNS):
-        start_result = start_workload()
-        start_server_process_times.append(
-            start_result["total"] - start_result["pretransfer"]
-        )
-        stop_result = stop_workload()
-        stop_server_process_times.append(
-            stop_result["total"] - stop_result["pretransfer"]
+        delete_to_name_lookup_times.append(delete_result["namelookup"])
+        delete_to_connect_times.append(
+            delete_result["connect"] - delete_result["namelookup"]
         )
 
-    print("Run start workload")
-    print(f"Avg: {mean(start_server_process_times) * 1_000}ms")
-    print(f"Median: {median(start_server_process_times) * 1_000}ms")
-
-    print("Run stop workload")
-    print(f"Avg: {mean(stop_server_process_times) * 1_000}ms")
-    print(f"Median: {median(stop_server_process_times) * 1_000}ms")
+    return {
+        f"Post {endpoint}": {
+            "server_process_times": post_to_server_process_times,
+            "name_lookup_times": post_to_name_lookup_times,
+            "connect_times": post_to_connect_times,
+        },
+        f"Delete {endpoint}": {
+            "server_process_times": delete_to_server_process_times,
+            "name_lookup_times": delete_to_name_lookup_times,
+            "connect_times": delete_to_connect_times,
+        },
+    }
 
 
 def execute_sql():
     add_database("db")
     server_process_times = []
+    name_lookup_times = []
+    connect_times = []
     for _ in range(RUNS):
-        result = post_sql("db")
-        server_process_times.append(result["total"] - result["pretransfer"])
-    print("Run execute sql")
-    print(f"Avg: {mean(server_process_times) * 1_000}ms")
-    print(f"Median: {median(server_process_times) * 1_000}ms")
+        results = post_sql("db")
+        server_process_times.append(results["total"] - results["pretransfer"])
+        name_lookup_times.append(results["namelookup"])
+        connect_times.append(results["connect"] - results["namelookup"])
 
     delete_database("db")
+
+    return {
+        "server_process_times": server_process_times,
+        "name_lookup_times": name_lookup_times,
+        "connect_times": connect_times,
+    }
+
+
+def get_enpoint_request():
+    results = get_endpoints()
+    for endpoint, result in results.items():
+        print_data(endpoint, result)
+
+
+def add_delete_database():
+    result = post_delete_on_endpoint("Database", add_database, delete_database, "db")
+    print_data("Add Database", result["Post Database"])
+    print_data("Delete Database", result["Delete Database"])
+
+
+def start_stop_worker():
+    result = post_delete_on_endpoint("Worker", start_worker, stop_worker)
+    print_data("Start Worker", result["Post Worker"])
+    print_data("Stop Worker", result["Delete Worker"])
+
+
+def start_stop_workload():
+    result = post_delete_on_endpoint("Workload", start_workload, stop_workload)
+    print_data("Start Workload", result["Post Workload"])
+    print_data("Stop Workload", result["Delete Workload"])
+
+
+def post_to_sql_endpoint():
+    result = execute_sql()
+    print_data("Execute sql", result)
 
 
 def run_benchmark():
     """Run benchmark."""
-    get_endpoints()
+    get_enpoint_request()
     add_delete_database()
     start_stop_worker()
     start_stop_workload()
