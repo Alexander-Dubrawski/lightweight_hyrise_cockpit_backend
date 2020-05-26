d21c9b66d611efbddcd2b057642b6780dbfe5341
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-05-26 19:17:55 +0200

    Add lua script for wrk formatting

---
+++ benchmark_tools/report.lua
@@ -0,0 +1,24 @@
+done = function(summary, latency, requests)
+    io.write("latency_values:\n")
+    io.write(string.format('{"min": %d, "max": %d, "mean": %d, "stdev": %d}\n', latency.min, latency.max, latency.mean, latency.stdev))
+    io.write("request_values:\n")
+    io.write(string.format('{"min": %d, "max": %d, "mean": %d, "stdev": %d}\n', requests.min, requests.max, requests.mean, requests.stdev))
+    io.write("latency_distribution:\n")
+    for _, p in pairs({50, 75.000, 90, 99.000, 99.900, 99.900 ,99.990, 99.999 }) do
+        n = latency:percentile(p)
+        io.write(string.format('"%g%%": %d, ', p, n))
+    end
+    io.write(string.format('"%g%%": %d }\n', 99.999999999, latency:percentile(99.999999999)))
+    percentiles = {} 
+    for i=1, 99 do
+        percentiles[i] = i
+    end
+    io.write("percentiles:\n")
+    io.write('{"percentiles": [')
+    for _, p in pairs(percentiles) do
+        n = latency:percentile(p)
+        io.write(string.format(" %d,",n))
+    end
+    io.write(string.format(" %d]}\n",latency:percentile(99.9999999)))
+end
+
