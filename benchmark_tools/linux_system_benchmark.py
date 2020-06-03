242229e6b83685aa6e3587944a80f1bee9ef5c20
Alexander Dubrawski <Alexander.Dubrawski@vm-momentum.eaalab.hpi.uni-potsdam.de>
2020-06-03 21:01:45 +0000

    Adjust linux script

--- benchmark_tools/linux_system_benchmark.py
+++ benchmark_tools/linux_system_benchmark.py
@@ -1,35 +1,41 @@
 from concurrent import futures
-from subprocess import check_output
+from datetime import datetime
+from subprocess import CalledProcessError, check_output
 from time import sleep
 
 from backend.settings import BACKEND_PORT, DB_MANAGER_PORT, GENERATOR_PORT
 
-DURATION = 40
-SLEEP_DURATION = 0.5
+DURATION = 2
+SLEEP_DURATION = 1
 
 
 def monitor_components(argument):
     component, pids = argument
     total_results = {component: []}
-    runs = 0
-    while runs < DURATION:
-        results = (
+    row_results = []
+    run = 0
+    while run < DURATION:
+        row_results.append(
             check_output(
                 f"ps -p {','.join(pids)}  -o pid,%cpu,%mem | ts '%Y-%m-%d_%H:%M:%S'",
                 shell=True,
             )
-            .decode("utf-8")
-            .splitlines()
         )
-        results.pop(0)
-        total_results[component] += [line.split() for line in results]
-        sleep(SLEEP_DURATION)
-        runs += SLEEP_DURATION
+        sleep(1)
+        run += SLEEP_DURATION
+
+    for row_result in row_results:
+        result = row_result.decode("utf-8").splitlines()
+        result.pop(0)
+        total_results[component] += [line.split() for line in result]
     return total_results
 
 
 def get_child_pids(ppid):
-    return check_output(f"pgrep -P {ppid}", shell=True).decode("utf-8").split()
+    try:
+        return check_output(f"pgrep -P {ppid}", shell=True).decode("utf-8").split()
+    except CalledProcessError:
+        return []
 
 
 def get_pids():
@@ -49,17 +55,66 @@ def get_pids():
         .split()[1]
     )
     return [
-        ("backened", [backened_ppid] + get_child_pids(backened_ppid)),
+        ("back_end", [backened_ppid] + get_child_pids(backened_ppid)),
         ("generator", [generator_ppid] + get_child_pids(generator_ppid)),
         ("manager", [manager_ppid] + get_child_pids(manager_ppid)),
     ]
+
+
+def avg_usage(data_set, index):
+    measurements = {
+        "usage": [],
+        "time_stamp": [],
+    }
+    last_ts = 0
+    for data in data_set:
+        current_ts = datetime.timestamp(data[0])
+        if current_ts > last_ts:
+            measurements["usage"].append(float(data[index]))
+            measurements["time_stamp"].append(current_ts)
+            last_ts = current_ts
+        else:
+            measurements["usage"][-1] = measurements["usage"][-1] + float(data[index])
+    return measurements
+
+
+def format_data(row_data):
+    formatted_data = {}
+    for component, results in row_data.items():
+        formatted_lines = []
+        for line in results:
+            formatted_line = line
+            formatted_line[0] = datetime.strptime(
+                formatted_line[0], "%Y-%m-%d_%H:%M:%S"
+            )
+            formatted_lines.append(formatted_line)
+        formatted_data[component] = formatted_lines
+    return formatted_data
 
 
 def run_benchmark():
     worker_thread = 3
     ppids = get_pids()
     with futures.ThreadPoolExecutor(worker_thread) as executor:
-        _ = executor.map(monitor_components, ppids)
+        res = executor.map(monitor_components, ppids)
+    results = list(res)
+    combined_res = {}
+    for result in results:
+        combined_res.update(result)
+    formatted_res = format_data(combined_res)
+
+    _ = {
+        "CPU": {
+            "back_end": avg_usage(formatted_res["back_end"], 2),
+            "generator": avg_usage(formatted_res["generator"], 2),
+            "manager": avg_usage(formatted_res["manager"], 2),
+        },
+        "MEMORY": {
+            "back_end": avg_usage(formatted_res["back_end"], 3),
+            "generator": avg_usage(formatted_res["generator"], 3),
+            "manager": avg_usage(formatted_res["manager"], 3),
+        },
+    }
 
 
 if __name__ == "__main__":
