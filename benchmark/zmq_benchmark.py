d76011ee5691398aaaeb8577f09678a2c0c32d70
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-06-06 22:52:51 +0200

    Add plotting

--- benchmark/zmq_benchmark.py
+++ benchmark/zmq_benchmark.py
@@ -1,16 +1,57 @@
 # type: ignore
+from calendar import timegm
 from concurrent import futures
 from statistics import mean, median, pstdev
-from time import time_ns
+from time import gmtime, time_ns
 
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.pyplot import figure
 from zmq import REQ, Context
 
 from backend.request import Header, Request
 from backend.settings import DB_MANAGER_HOST, DB_MANAGER_PORT
 
-CLIENTS = [2, 4]
-RUNS = 10
+CLIENTS = [2, 4, 8, 16, 32, 40]
+RUNS = 100_000
+PERCENTILES = [1, 25, 50, 75.000, 90, 99.000, 99.900, 99.990, 99.999]
+
+
+def plot_hdr_histogram(results, file_name):
+    fig = figure(num=None, figsize=(40, 30), dpi=300, facecolor="w", edgecolor="k")
+    plt.rcParams.update({"font.size": 22})
+    col_labels = [f"{percentile}th" for percentile in PERCENTILES]
+    rows = []
+    row_labels = []
+    for number, data in results.items():
+        row_labels.append(f"clients: {number}")
+        row = []
+        y_values = []
+        x_values = [str(percentile) for percentile in PERCENTILES]
+        for value in data["latency distribution"]:
+            y_values.append(value)
+            row.append(value)
+        rows.append(row)
+        plt.plot(
+            x_values, y_values, label=f"clients: {number}", linewidth=4.0,
+        )
+    plt.legend()
+    plt.ylabel("Latency (milliseconds)")
+    plt.xlabel("Percentile")
+    plt.title("Latency by Percentile Distribution")
+    plt.grid()
+    plt.table(
+        cellText=rows,
+        rowLabels=row_labels,
+        cellLoc="center",
+        colLabels=col_labels,
+        loc="bottom",
+        bbox=[0, -0.29, 1, 0.17],
+    )
+    plt.subplots_adjust(left=0.2, bottom=0.2)
+    ts = timegm(gmtime())
+    plt.savefig(f"measurements/{file_name}_{ts}.pdf")
+    plt.close(fig)
 
 
 def run_clinet(runs):
@@ -40,14 +81,15 @@ def claculate_values(args):
     median_latency = median(complete_latency)
     stdev_latency = pstdev(complete_latency)
     num = np.array(complete_latency)
-    percentiles = [1, 25, 50, 75.000, 90, 99.000, 99.900, 99.990]
-    percentiles_values = [np.percentile(num, percentile) for percentile in percentiles]
+    percentiles_values = [np.percentile(num, percentile) for percentile in PERCENTILES]
     return {
         number_clinets: {
             "Avg": round(avg_latency / 1_000_000, 3),
             "Median": round(median_latency / 1_000_000, 3),
             "Stdev": round(stdev_latency / 1_000_000, 3),
-            "latency distribution": percentiles_values,
+            "latency distribution": [
+                round(val / 1_000_000, 3) for val in percentiles_values
+            ],
         }
     }
 
@@ -79,7 +121,7 @@ def run_benchmark():
 def main():
     row_results = run_benchmark()
     formatted_results = run_calculations(row_results)
-    print(formatted_results)
+    plot_hdr_histogram(formatted_results, "zmq_hdr")
 
 
 if __name__ == "__main__":
