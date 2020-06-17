fca8311af6d4cfe3e31aae8945fd8dbc0dd0b126
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-06-17 18:15:07 +0200

    Add argument parser

--- backend/database_manager/cli.py
+++ backend/database_manager/cli.py
@@ -1,4 +1,6 @@
 """CLI used to start the database manager."""
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
 from backend.settings import (
     BROKER_LISTENING,
     BROKER_PORT,
@@ -12,12 +14,50 @@ from backend.settings import (
 
 from .broker import Broker
 
-NUMBER_WORKER = 1
-NUMBER_THREAD = 1
+
+class Parser:
+    """Parse arguments from command line."""
+
+    def __init__(self):
+        """Initialize a ArgumentParser."""
+        self.parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        self._add_arguments()
+
+    def _add_arguments(self):
+        self.parser.add_argument(
+            "--worker",
+            "-w",
+            dest="worker",
+            metavar="",
+            type=int,
+            nargs="?",
+            help="number workers",
+        )
+        self.parser.add_argument(
+            "--threads",
+            "-t",
+            dest="threads",
+            metavar="",
+            type=str,
+            nargs="?",
+            help="number threads for each worker",
+        )
+
+    def get_configuration(self):
+        """Return argument configuration."""
+        worker = self.parser.parse_args().worker
+        threads = self.parser.parse_args().threads
+        return {
+            "worker": int(worker),
+            "threads": int(threads),
+        }
 
 
 def main() -> None:
     """Create and start a database manager."""
+    argp = Parser()  # type: ignore
+    config = argp.get_configuration()
+
     try:
         with Broker(
             DB_MANAGER_LISTENING,
@@ -28,8 +68,8 @@ def main() -> None:
             WORKER_PORT,
             WORKLOAD_SUB_HOST,
             WORKLOAD_PUBSUB_PORT,
-            NUMBER_WORKER,
-            NUMBER_THREAD,
+            mumber_worker=config["worker"],
+            number_threads=config["threads"],
         ) as manager_broker:
             print(f"Broker running on port {BROKER_PORT} (Press CTRL+C to quit)")
             manager_broker.run()
