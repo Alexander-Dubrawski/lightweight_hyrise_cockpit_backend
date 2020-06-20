04fbd1b5ba7c4abc71c01764e67167b658aa448a
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-06-20 15:53:59 +0200

    Adjust broker for load balancing

--- backend/database_manager/broker.py
+++ backend/database_manager/broker.py
@@ -1,15 +1,14 @@
-from json import loads
+from json import dumps, loads
 from multiprocessing import Process
 from threading import Thread
 from time import sleep
 from types import TracebackType
 from typing import List, Optional, Type
 
-from zmq import DEALER, POLLIN, REP, ROUTER, Context, Poller, proxy
-
 from backend.database_manager.manager import DatabaseManager
 from backend.request import Body, Request
 from backend.response import Response, get_response
+from zmq import DEALER, IDENTITY, POLLIN, REQ, ROUTER, Context, Poller
 
 
 def _call_metric(body: Body) -> Response:
@@ -23,30 +22,100 @@ def worker_thread(url_thread, context=None):
     server_calls = {
         "get metric": _call_metric,
     }
-    context = context or Context.instance()
-    socket = context.socket(REP)
+    context = Context.instance()
+    socket = context.socket(REQ)
     socket.connect(url_thread)
+    # Tell broker we're ready for work
+    socket.send(b"READY")
 
     while True:
-        request = socket.recv_json()
+        (
+            broker_address,
+            empty_frame,
+            client_address,
+            empty_frame,
+            client_request,
+        ) = socket.recv_multipart()
+        request = loads(client_request.decode("utf-8"))
         call = request["header"]["message"]
         func = server_calls[call]
-        socket.send_json(func(request["body"]))
+        results = func(request["body"])
+        formatted_results = dumps(results)
+        socket.send_multipart(
+            [broker_address, b"", client_address, b"", formatted_results.encode()]
+        )
 
 
-def worker_proxy(number_threads, url_broker):
-    url_thread = "inproc://threads"
+def worker_proxy(broker_id, number_threads, worker_id, url_broker):
     context = Context.instance()
     broker = context.socket(ROUTER)
+    byte_worker_id = f"WORKER_{worker_id}".encode()
+    broker.setsockopt(IDENTITY, byte_worker_id)
     broker.connect(url_broker)
-    threads = context.socket(DEALER)
-    threads.bind(url_thread)
+    thread = context.socket(ROUTER)
+    thread.bind("inproc://threads")
+    sleep(5)
 
+    threads_obj = []
     for i in range(number_threads):
-        thread = Thread(target=worker_thread, args=(url_thread,))
-        thread.start()
+        t = Thread(target=worker_thread, args=("inproc://threads",))
+        t.start()
+        threads_obj.append(t)
 
-    proxy(broker, threads)
+    poller = Poller()
+    poller.register(thread, POLLIN)
+    threads = []
+    broker.send_multipart([broker_id, b"", b"READY"])
+    poller.register(broker, POLLIN)
+
+    while True:
+        sockets = dict(poller.poll())
+
+        if thread in sockets:
+            # Handle worker activity on the backend
+            request = thread.recv_multipart()
+            # we need to unpack the last three values. This is the inner envelop
+            thread_address, empty_frame, broker_address = request[:3]
+            threads.append(thread_address)
+            if broker_address != b"READY" and len(request) > 3:
+                # If client reply, send rest back to broker
+                (
+                    thread_address,
+                    empty_frame,
+                    broker_address,
+                    empty_frame,
+                    client_address,
+                    empty_frame,
+                    cleint_reply,
+                ) = request
+                broker.send_multipart(
+                    [broker_address, b"", client_address, b"", cleint_reply]
+                )
+
+        if broker in sockets:
+            # Get next client request, route to last-used worker
+            message = broker.recv_multipart()
+            (
+                broker_address,
+                empty_frame,
+                client_address,
+                empty_frame,
+                client_request,
+            ) = message
+            thread_address = threads.pop(0)
+            if threads:
+                broker.send_multipart([broker_id, b"", b"READY"])
+            thread.send_multipart(
+                [
+                    thread_address,
+                    b"",
+                    broker_address,
+                    b"",
+                    client_address,
+                    b"",
+                    client_request,
+                ]
+            )
 
 
 def start_database_manager(
@@ -123,13 +192,14 @@ class Broker:
         self._context = Context(io_threads=io_threads)
         self._client = self._context.socket(ROUTER)
         self._client.bind("tcp://127.0.0.1:{:s}".format(self._broker_port))
-        self._worker = self._context.socket(DEALER)
+        self._worker = self._context.socket(ROUTER)
+        self._worker.setsockopt(IDENTITY, b"BROKER")
         self._worker.bind("tcp://127.0.0.1:{:s}".format(self._worker_port))
         self._database_manager = self._context.socket(DEALER)
         self._database_manager.bind(
             "tcp://127.0.0.1:{:s}".format(self._db_manager_port)
         )
-
+        self._worker_addresses: List = []
         self._poller = Poller()
         self._poller.register(self._client, POLLIN)
         self._poller.register(self._worker, POLLIN)
@@ -138,30 +208,40 @@ class Broker:
     def _start_worker(self, number_worker, number_threads, url_broker) -> List:
         workers = []
         for i in range(number_worker):
-            worker = Process(target=worker_proxy, args=(number_threads, url_broker,))
+            worker = Process(
+                target=worker_proxy, args=(b"BROKER", number_threads, i, url_broker,)
+            )
             worker.start()
             workers.append(worker)
         return workers
 
     def run(self) -> None:
         """Start the server loop."""
+
         while True:
             socks = dict(self._poller.poll())
 
+            if self._worker in socks:
+                request = self._worker.recv_multipart()
+                worker_address, empty_frame, client_adress = request[:3]
+                self._worker_addresses.append(worker_address)
+                if client_adress != b"READY" and len(request) > 3:
+                    # If client reply, send rest back to front end
+                    empty_frame, cleint_reply = request[3:]
+                    self._client.send_multipart([client_adress, b"", cleint_reply])
+
             if socks.get(self._client) == POLLIN:
                 multi_part_message = self._client.recv_multipart()
-                identity, delimeter_fram, data = multi_part_message
-                request: Request = loads(data.decode("utf-8"))
-                call = request["header"]["message"]
+                client_adress, empty_frame, client_request = multi_part_message
+                req: Request = loads(client_request.decode("utf-8"))
+                call = req["header"]["message"]
                 if call in self._database_manager_calls:
-                    print(call)
                     self._database_manager.send_multipart(multi_part_message)
                 else:
-                    self._worker.send_multipart(multi_part_message)
-
-            if socks.get(self._worker) == POLLIN:
-                message = self._worker.recv_multipart()
-                self._client.send_multipart(message)
+                    worker_address = self._worker_addresses.pop(0)
+                    self._worker.send_multipart(
+                        [worker_address, b"", client_adress, b"", client_request]
+                    )
 
             if socks.get(self._database_manager) == POLLIN:
                 message = self._database_manager.recv_multipart()
